dc6ca3c37da5e53d07634b15f95282ea2febd271
al299993@dal.ca <alishahzodov@gmail.com>
2025-01-26 14:10:23 -0400

    Bubble code done fr

--- Assets/Bubble/BubbleResizing.cs
+++ Assets/Bubble/BubbleResizing.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
 public class BubbleResizing : MonoBehaviour
 {
     [Header ("Bubble")]
     SpriteRenderer bubble;
+    ParticleSystem particle;
     Vector2 originalSize;
     Vector2 maxSizeVector;
+    Vector3 sizeCalculated;
     public float maxSize;
     public float growSpeed;
-
+    public float timeRemaining = 3.0f;
+    public bool timerIsRunning = false;
+    public float bubbleCooldown = 3.0f;
+    public bool bubbleCooldownOn = false;
     [Header ("Gravity")]
     Rigidbody2D guyRigidbody;
     public float fallSpeedReduction;
@@ -19,31 +27,39 @@ public class BubbleResizing : MonoBehaviour
         bubble = this.GetComponent<SpriteRenderer>();
         guyRigidbody = this.transform.parent.GetComponent<Rigidbody2D>();
         gravity = guyRigidbody.gravityScale;
+        particle = this.GetComponent<ParticleSystem>();
     }
     void Start()
     {
         originalSize = transform.localScale;
         maxSizeVector = originalSize*maxSize;
+        timeRemaining = 3.0f;
     }
 
     public void Grow()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && !bubbleCooldownOn)
         {
             isKeyDown = true;
+            particle.Stop();
         }
         if(Input.GetKeyUp(KeyCode.Space))
         {
             isKeyDown = false;
+            transform.localScale = originalSize; 
+            timerIsRunning = false;
         }
         if (isKeyDown)
         {
-            Debug.Log("!");
             bubble.enabled = true;
             AdjustFallSpeed();
             if (transform.localScale.x < (maxSizeVector.x))
             {
                 transform.localScale = transform.localScale*(1+(growSpeed/10)*Time.deltaTime);
+                if(transform.localScale.x >= 0.02f)
+                {
+                    timerIsRunning = true;
+                }
             }
         }
         else bubble.enabled = false;
@@ -55,8 +71,53 @@ public class BubbleResizing : MonoBehaviour
             guyRigidbody.gravityScale = guyRigidbody.gravityScale*(1-(fallSpeedReduction/10)*Time.deltaTime);
         }
     }
+    void timerEnded()
+    {
+        Debug.Log("Timer Ends");
+        particle.Play();
+        timeRemaining = 3.0f;
+        transform.localScale = originalSize;
+        isKeyDown = false;
+        bubbleCooldownOn = true;
+    }
+
+    void bubbleCooldownTimer()
+    {
+        Debug.Log("Bubble Cooldown Off");
+        bubbleCooldown = 3.0f;
+        bubbleCooldownOn = false;
+    }
     void Update()
     {
+        sizeCalculated = this.GetComponent<Renderer>().bounds.size;
+        if(bubbleCooldownOn){
+            if (bubbleCooldown > 0 )
+            {
+                bubbleCooldown -= Time.deltaTime;
+            }
+            else
+            {
+                bubbleCooldown = 0;
+                bubbleCooldownOn = false;
+            }
+        }
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                timerEnded();
+            }
+        }
+        else
+        {
+            timeRemaining = 3.0f;
+        }
        Grow();
     }
 }
